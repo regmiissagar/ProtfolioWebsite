2670e24a61fab565d992eeb35e0f950c17e7abd3
regmiissagar <106870304+regmiissagar@users.noreply.github.com>
2022-06-04 14:35:41 +0200

    my portfolio website

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addclass("sticky");
+            
+        }else{
+            $('.navbar').removeclass("sticky");
+        }
+    }
+
+}
